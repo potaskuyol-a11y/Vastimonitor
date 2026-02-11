89117d3224ded658dc34ee125267c76312626618
potaskuyol-a11y <potaskuyol@hotmail.com>
2026-02-11 18:05:59 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,6 +4,9 @@ import os
 TG_TOKEN = os.getenv("TG_TOKEN")
 CHAT_ID = os.getenv("TG_CHAT")
 
+MAX_PRICE = 1.0   # временно поставим 1$, чтобы протестировать
+GPU_NAME = "A100"  # временно ставим A100 для проверки
+
 def send_message(text):
     url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
     requests.get(url, params={"chat_id": CHAT_ID, "text": text})
@@ -11,16 +14,35 @@ def send_message(text):
 def check_vast():
     url = "https://console.vast.ai/api/v0/bundles"
 
-    try:
-        r = requests.get(url)
+    r = requests.get(url)
+    data = r.json()
 
-        print("STATUS CODE:", r.status_code)
-        print("HEADERS:", r.headers)
-        print("RESPONSE START:")
-        print(r.text[:1500])  # первые 1500 символов
+    if "offers" not in data:
+        return
 
-    except Exception as e:
-        print("ERROR:", str(e))
+    filtered = [
+        offer for offer in data["offers"]
+        if GPU_NAME.lower() in offer["gpu_name"].lower()
+    ]
+
+    if not filtered:
+        print("Нет GPU найдено")
+        return
+
+    cheapest = sorted(filtered, key=lambda x: x["dph_total"])[0]
+
+    price = cheapest["dph_total"]
+    gpu = cheapest["gpu_name"]
+    location = cheapest["geolocation"]
+
+    print("Найдено:", gpu, price)
+
+    if price <= MAX_PRICE:
+        send_message(
+            f"🔥 Найден {gpu}\n"
+            f"Цена: ${price}/час\n"
+            f"Локация: {location}"
+        )
 
 if __name__ == "__main__":
     check_vast()
